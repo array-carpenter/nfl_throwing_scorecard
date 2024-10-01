a611734962919a84fdeaa7436ea03abae3ee8689
Ray Carpenter <55845213+array-carpenter@users.noreply.github.com>
2024-10-01 09:02:02 -0400

    Added Huntley and Rudolph

--- throwing_summary.py
+++ throwing_summary.py
@@ -54,7 +54,7 @@ player_info = qb_info[qb_info['Name'] == player_name].iloc[0]
 formatted_player_name = player_info['data_formatted_player_name']
 
 # Assuming the game date is stored in a specific format (e.g., 'YYYY-MM-DD')
-game_date_str = '2024-09-29'  # Adjust as necessary for the game being summarized
+game_date_str = '2024-09-30'  # Adjust as necessary for the game being summarized
 game_date = datetime.strptime(game_date_str, '%Y-%m-%d')
 
 # Calculate the player's age based on their birthday and the game date
@@ -129,8 +129,8 @@ passing_plays = filtered_df[filtered_df['passer_player_name'] == formatted_playe
 rushing_plays = filtered_df[filtered_df['rusher_player_name'] == formatted_player_name]
 
 # Filter game data by game id
-game_data_passing = passing_plays[passing_plays['game_id'] == '2024_04_BUF_BAL'] ### follow format YEAR_WEEK_AWAY_HOME 2023_12_BUF_PHI
-game_data_rushing = rushing_plays[rushing_plays['game_id'] == '2024_04_BUF_BAL']
+game_data_passing = passing_plays[passing_plays['game_id'] == '2024_04_SEA_DET'] ### follow format YEAR_WEEK_AWAY_HOME 2023_12_BUF_PHI
+game_data_rushing = rushing_plays[rushing_plays['game_id'] == '2024_04_SEA_DET']
 
 # Calculate cumulative completions and attempts for passing plays
 game_data_passing['cumulative_completions'] = game_data_passing['complete_pass'].cumsum()
@@ -320,7 +320,7 @@ def qb_dashboard(game_data_passing: pd.DataFrame, headshot: Image, team_abb: str
     # Adjust biographical information display
     ax_bio.text(0.5, 0.95, player_name, fontsize=22, ha='center', fontweight='bold')  # Dynamically display QB name
     ax_bio.text(0.5, 0.50, f'{player_info["dexterity"]}HQB, Age: {player_age}, {player_height}/{player_weight} lbs', fontsize=18, ha='center')  # Dynamically display height and weight
-    ax_bio.text(0.5, 0.1, f'2024 Week 4 Throwing Summary vs. Buffalo', fontsize=18, ha='center', fontstyle='italic')  # Still manually set game information
+    ax_bio.text(0.5, 0.1, f'2024 Week 4 Throwing Summary vs. Seattle', fontsize=18, ha='center', fontstyle='italic')  # Still manually set game information
     ax_bio.axis('off')
 
   
@@ -407,4 +407,4 @@ def qb_dashboard(game_data_passing: pd.DataFrame, headshot: Image, team_abb: str
 
 save_path = 'qb_dashboard.png'
 
-qb_dashboard(game_data_passing, headshot, "BAL", summary_df, pass_distance_summary, quarter_positions, save_path=save_path)
+qb_dashboard(game_data_passing, headshot, "DET", summary_df, pass_distance_summary, quarter_positions, save_path=save_path)
